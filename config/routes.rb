a6002850d35c887b2999b2dad1a6ccca9e342364
Motoyuki Koseki <motoyuki.koseki@gmail.com>
2021-01-08 14:31:16 +0000

    [Add]管理者ログイン画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :admins, module: "admins"
   devise_for :customers, module: "customers"
+
+  namespace :admin do
+    root to: "homes#top"
+    resources :items
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
 end
