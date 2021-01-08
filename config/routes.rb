8a081ffafe8070a398fe9e6a73bce7e4e95ef177
Motoyuki Koseki <motoyuki.koseki@gmail.com>
2021-01-08 14:18:26 +0000

    [Update]deviseのルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  devise_for :admins, module: "admins"
+  devise_for :customers, module: "customers"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
