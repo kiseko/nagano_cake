d9102518b7fe223a0bc174588f1d9426530c82aa
Motoyuki Koseki <motoyuki.koseki@gmail.com>
2021-01-08 14:34:51 +0000

    [Add]商品新規登録画面の作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,37 @@
 class Admin::ItemsController < ApplicationController
+
+  def new
+    @item = Item.new
+    @genres = Genre.all
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
+  end
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
+  end
+
 end
