31556b60936e9c74522d6ce0306f10043002ca92
Motoyuki Koseki <motoyuki.koseki@gmail.com>
2021-01-09 17:25:36 +0000

    [Add]商品一覧画面(会員側)の作成

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
     resources :genres, only: [:create, :index, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
   end
+  
+  scope module: :public do
+    resources :items, only: [:index, :show]
+  end
+    
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
