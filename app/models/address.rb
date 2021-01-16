71ee58c70aac6bc2ec499ce28adc47e32c704ba9
Motoyuki Koseki <motoyuki.koseki@gmail.com>
2021-01-16 00:52:12 +0000

    [Add]カート画面(商品がない場合)を作成

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,7 +4,7 @@ class Address < ApplicationRecord
 
   validates :customer_id, presence: true
   validates :name, presence: true
-  validates :postal_code, presence: true
+  validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
   validates :address, presence: true
 
   def registration_address_info
