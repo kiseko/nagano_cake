175dc009885660bfcfb03d7b6aa6972f8aa22b6c
Motoyuki Koseki <motoyuki.koseki@gmail.com>
2021-01-17 17:32:21 +0000

    [Update]ジャンル別で商品一覧を表示できるように変更

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -11,4 +11,16 @@ class Public::ItemsController < ApplicationController
     @cart_item = CartItem.new
   end
 
+  def search
+    if params[:search_genre][:select_genre].empty?
+      redirect_to items_path
+    else
+      @genres = Genre.all
+      @genre = Genre.find(params[:search_genre][:select_genre])
+      @select_items = Item.where(genre_id: params[:search_genre][:select_genre])
+      @items = @select_items.page(params[:page]).per(12)
+      render :index
+    end
+  end
+
 end
